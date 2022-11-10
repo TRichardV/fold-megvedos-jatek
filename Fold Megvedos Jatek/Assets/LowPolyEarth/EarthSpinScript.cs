a081576119b0a2c89b1a870f1efe893f44264997
TRichardV <tillinger.richard_viktor@liveedu.mathiasz.hu>
2022-11-10 22:54:38 +0100

    graphics scene

---
+++ Fold Megvedos Jatek/Assets/LowPolyEarth/EarthSpinScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthSpinScript : MonoBehaviour {
+    public float speed = 10f;
+
+    void Update() {
+        transform.Rotate(-speed/4 * Time.deltaTime, -speed * Time.deltaTime, 0);
+    }
+}
