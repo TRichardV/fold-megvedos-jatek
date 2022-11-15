afa9840e91c7ae1e7b36c04a415b7d6486fc4cf4
TRichardV <tillinger.richard_viktor@liveedu.mathiasz.hu>
2022-11-15 23:14:06 +0100

    meteorok fixálása

---
+++ Fold Megvedos Jatek/Assets/GraphicsFiles/Asteroids/Asteroid_rotation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid_rotation : MonoBehaviour
+{
+    public GameObject[] array;
+    private void Start()
+    {
+        
+    }
+}
