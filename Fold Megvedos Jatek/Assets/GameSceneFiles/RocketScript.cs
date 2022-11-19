7eb67ff5ccdf98fd9452c28d547bc8569645f644
TRichardV <tillinger.richard_viktor@liveedu.mathiasz.hu>
2022-11-19 23:31:46 +0100

    robbanás effekt

--- Fold Megvedos Jatek/Assets/GameSceneFiles/RocketScript.cs
+++ Fold Megvedos Jatek/Assets/GameSceneFiles/RocketScript.cs
@@ -21,6 +21,8 @@ public class RocketScript : MonoBehaviour {
     readonly float rangeOutX = 5f;
     readonly float rangeOutY = 10f;
 
+    ParticleSystem explosionParticle;
+    bool isExploded = false;
 
     public Transform RocketP;
 
@@ -48,7 +50,10 @@ public class RocketScript : MonoBehaviour {
 
     void FixedUpdate() {
 
-        RocketP.position = new Vector2(RocketP.position.x + kX, RocketP.position.y + kY);
+        if (!isExploded)
+        {
+            RocketP.position = new Vector2(RocketP.position.x + kX, RocketP.position.y + kY);
+        }
 
         if (RocketP.position.x > rangeOutX || RocketP.position.x < -rangeOutX ||
             RocketP.position.y > rangeOutY || RocketP.position.y < -rangeOutY) {
@@ -65,10 +70,23 @@ public class RocketScript : MonoBehaviour {
 
             collision.gameObject.GetComponent<MeteorScript>().shot(damage);
             GameObject.Find("Main Camera").GetComponent<Shake>().start = true;
-            Destroy(this.gameObject);
 
+            transform.GetChild(0).gameObject.SetActive(false);
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<BoxCollider2D>().enabled = false;
+            explosionParticle = transform.GetComponentInChildren<ParticleSystem>();
+            explosionParticle.Play();
+
+            StartCoroutine(Destroyer());
         }
 
+    }
+
+    IEnumerator Destroyer()
+    {
+        isExploded = true;
+        yield return new WaitForSeconds(explosionParticle.main.duration);
+        Destroy(this.gameObject);
     }
 
 }
